3a160c9ec01bd4cb7640015989165fae0debfeb1
jrlopez14 <jrlopez14@miners.utep.edu>
2020-09-21 12:43:21 -0600

    Implemented own string comparison and string to number functions

--- src/uimain.c
+++ src/uimain.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // for strcmp
-#include <ctype.h>   // for atoi, used to convert string to integer to get an index
 #include "tokenizer.h"
 #include "history.h"
 
 #define MAX_LENGTH 32 // Max size of string to be given by user
 
+int strcmp(char *str1, char *str2);
+int str_to_num(char *str);
+
 int main()
 {
-  
+  printf("%d", strcmp("history", "hello"));
   List *history = init_history(); // Initiates history list
   char **tokens, str[MAX_LENGTH], c; // Initializes tokens, str for input from user, and c to
                                      // read input into str 
@@ -18,8 +19,9 @@ int main()
   
   printf("\tWelcome to the tokenizer!");
   printf("\n--------------------------------------\n");
-  printf("Special commands: \"q\" to quit\n");
-  printf("                  \"!n\" to get string of item with id:n\n");
+  printf("Special commands: \"exit\" to stop\n");
+  printf("                  \"history\"\n");
+  printf("                  \"!n\" for nth item in history\n");
   printf("Enter a string to tokenize \n");
   while(strcmp(str,"q") != 0){ 
     printf("\n> ");
@@ -29,11 +31,15 @@ int main()
       i++;
     }
     str[i] = '\0';                   // Stores input into str
-    if(strcmp(str,"q") == 0){        // If "q" is entered, exit loop
+    if(strcmp(str,"exit") == 0){        // If "q" is entered, exit loop
       goto done;
     }
+    else if(strcmp("history",str) == 0){
+      printf("%d", strcmp("history", str));
+      print_history(history);
+    }
     else if(str[0] == '!'){
-      index  = atoi(str+1);          // If string begins with !, get string of item
+      index  = str_to_num(str+1);          // If string begins with !, get string of item
       printf("%s", get_history(history,index)); // with id of the number that follows !
     }
     else{
@@ -44,8 +50,25 @@ int main()
   }  
  done:
   if(history->root != NULL) free_tokens(tokens);
-  printf("\n\tHistory:\n");
-  print_history(history); // prints history
   free_history(history);  // frees history  
   return 0;
+}
+
+int strcmp(char *str1, char *str2)
+{
+  int i;
+  for(i = 0; str1[i] == str2[i]; i++){
+    if( str1[i] == '\0') return 0;
+  }
+  return str1[i] - str2[i];
+}
+int str_to_num(char *str){
+  int number = 0;
+  int i = 0;
+  while(str[i] != '\0'){
+    number = number * 10;
+    number = number + (str[i]-'0');
+    i++;
+  }
+  return number;
 }
